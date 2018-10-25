7a8eb4ab2558e6d881abeedb8fcabe88dbdf27a9
tamat185 <tamat185@gmail.com>
2018-10-25 14:58:19 +0300

    Decorator of cash_func with change

--- 01.Python/01.Decorators/02.cash_func.py
+++ 01.Python/01.Decorators/02.cash_func.py
@@ -1,24 +1,26 @@
 def cash (func):
     cash_list = {}
-    def wrapper(*args):
+    def wrapper(*args,**kwargs):
         nonlocal cash_list
-        if args in cash_list.keys():
-            print(cash_list)
-            return cash_list[args]
+        if (tuple(args) + tuple(kwargs.values())) in cash_list:
+            return cash_list[(tuple(args) + tuple(kwargs.values()))]
         else:
-            result_func = func(*args)
-            cash_list[args] = result_func
-            print(cash_list)
+            result_func = func(*args,**kwargs)
+            cash_list[(tuple(args) + tuple(kwargs.values()))] = result_func
             return result_func
     return wrapper
 
 @cash
-def suma(*args):
+def suma(*args, **kwargs):
     total = 0
     for arg in args:
         total = total + arg
+    for key in kwargs:
+        total = total + kwargs[key]
     return total
 
 suma(1,2)
 suma(1,3)
 suma(1,2)
+suma(1,4)
+suma(1,a=4)
